aee6462d74c81d099f2f90802223e9a3c9b9642d
PieterQQ <pieter444@wp.pl>
2020-06-27 12:45:27 +0200

    Initial commit

---
+++ PrescriptionOnline/PrescriptionOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PrescriptionOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
