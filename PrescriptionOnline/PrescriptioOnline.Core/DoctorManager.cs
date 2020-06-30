f050f3c83ba37e1e6bd97d1b7a7c8ab8ea252a3f
PieterQQ <pieter444@wp.pl>
2020-06-30 09:33:23 +0200

    Add stuff in DTO

---
+++ PrescriptionOnline/PrescriptioOnline.Core/DoctorManager.cs
@@ -0,0 +1,76 @@
+﻿using PrescriptioOnline.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrescriptioOnline.Core
+{
+    public class DoctorManager : IDoctorManager
+    {
+        private readonly IDoctorRepository _doctorRepository;
+        private readonly IPrescriptionRepository _prescriptionRepository;
+        private readonly IMedicineRepository _medicineRepository;
+        private readonly DoctorsMappers _doctorsMappers;
+        public DoctorManager(IDoctorRepository doctorRepository, IPrescriptionRepository prescriptionRepository, IMedicineRepository medicineRepository, DoctorsMappers doctorsMappers)
+        {
+            _doctorRepository = doctorRepository;
+            _prescriptionRepository = prescriptionRepository;
+            _medicineRepository = medicineRepository;
+            _doctorsMappers = doctorsMappers;
+        }
+        public IEnumerable<DoctorDTO> GetAllDoctors(string filterstring)
+        {
+            var doctorEntities = _doctorRepository.GetAllDoctors();
+            if (!string.IsNullOrEmpty(filterstring))
+            {
+                doctorEntities = doctorEntities
+                    .Where(x => x.FirstName.ToLower().Contains(filterstring.ToLower()) || x.LastName.ToLower().Contains(filterstring.ToLower()));
+            }
+            return _doctorsMappers.Map(doctorEntities);
+        }
+        public IEnumerable<PrescriptionDTO> GetAllPrescriptionForADoctor(int doctorId, string filterstring)
+        {
+            var prescriptionEntities = _prescriptionRepository.GetAllPrescriptions().Where(x => x.DoctorId == doctorId);
+            if (!string.IsNullOrEmpty(filterstring))
+            {
+                prescriptionEntities = prescriptionEntities
+                    .Where(x => x.Name.ToLower().Contains(filterstring.ToLower()));
+            }
+            return _doctorsMappers.Map(prescriptionEntities);
+        }
+        public IEnumerable<MedicineDTO> GetAllMedicineForAPrescription(int prescriptionId, string filterstring)
+        {
+            var medicineEntities = _medicineRepository.GetAllMedicines().Where(x => x.PrescriptionId == prescriptionId);
+            if (!string.IsNullOrEmpty(filterstring))
+            {
+                medicineEntities = medicineEntities
+                    .Where(x => x.Name.ToLower().Contains(filterstring.ToLower())
+                             || x.ActiveSubstance.ToLower().Contains(filterstring.ToLower())
+                             || x.Producent.ToLower().Contains(filterstring.ToLower()));
+            }
+            return _doctorsMappers.Map(medicineEntities);
+        }
+        public void AddNewMedicine(MedicineDTO medicine,int prescriptionId)
+        {
+            var entity = _doctorsMappers.Map(medicine);
+            entity.PrescriptionId = prescriptionId;
+            _medicineRepository.AddNew(entity);
+
+        }
+        public void AddNewPrescription(PrescriptionDTO prescription, int DoctorId)
+        {
+            var entity = _doctorsMappers.Map(prescription);
+            entity.DoctorId = DoctorId;
+            _prescriptionRepository.AddNew(entity);
+
+           
+        }
+        public void AddNewDoctor(DoctorDTO doctor)
+        {
+            var entity = _doctorsMappers.Map(doctor);
+            _doctorRepository.AddNew(entity);
+
+        }
+    }
+}
